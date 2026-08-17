33f0ed37c129c7d7ffa26dbc1c10c2f4f331c260
Aadam <aadamaftabpersonal@gmail.com>
2026-08-17 10:27:23 +0530

    binsearchmed2

---
+++ medium/findmininrotatedsorted.cpp
@@ -0,0 +1,22 @@
+//sorted matlab binary search
+class Solution {
+public:
+    int findMin(vector<int>& nums){
+        int n = nums.size();
+        int low = 0, high = n-1, ans = INT_MAX;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]>nums[high]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+            ans = min(ans, nums[mid]);
+        }
+        return ans;
+    }
+};
+
+//if nums[mid]>nums[high], sure that array is rotated
+//for a normal array -> nums[low]<nums[mid]<nums[high]
